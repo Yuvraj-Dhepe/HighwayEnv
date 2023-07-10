22202e919d27453ee055e785d16ef8fba5140fe0
Yuvraj-Dhepe <yuvi.kiit@gmail.com>
2023-07-10 10:39:17 +0200

    Simple Env Y addition

--- highway_env/envs/__init__.py
+++ highway_env/envs/__init__.py
@@ -9,6 +9,7 @@ from highway_env.envs.u_turn_env import *
 from highway_env.envs.exit_env import *
 from highway_env.envs.racetrack_env import *
 from highway_env.envs.cust_env_m import *
+from highway_env.envs.simple_env import *
 from highway_env.envs.cust_env_y import *
 from gymnasium.envs.registration import register
 
@@ -20,4 +21,9 @@ register(
 register(
     id='rt-y-v0',
     entry_point='highway_env.envs:CustEnvY'
+)
+
+register(
+    id='rt-simple-v0',
+    entry_point='highway_env.envs:SimpleEnv'
 )
