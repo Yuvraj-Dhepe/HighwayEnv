bbd4bd5725a8792cfe94d301bdb41eb007e739a2
Nicola Horst <62853202+NicolaHorst@users.noreply.github.com>
2023-08-08 13:04:27 +0200

    nicolas chnages

--- highway_env/envs/nicola_custom_env.py
+++ highway_env/envs/nicola_custom_env.py
@@ -8,6 +8,7 @@ from highway_env.road.lane import StraightLane, CircularLane, SineLane, LineType
 import numpy as np
 
 from highway_env.vehicle.behavior import IDMVehicle
+from highway_env.vehicle.kinematics import Vehicle
 
 
 def make_nodes(single_nodes: List[str]) -> List[tuple]:
@@ -24,6 +25,7 @@ class CustomRoadEnv(AbstractEnv):
         config.update({
             "observation": {
                 "type": "OccupancyGrid",
+                "vehicles_count": 8,
                 "features": ['presence', 'on_road'],
                 "grid_size": [[-18, 18], [-18, 18]],
                 "grid_step": [3, 3],
@@ -34,7 +36,7 @@ class CustomRoadEnv(AbstractEnv):
                 "type": "DiscreteAction",
                 "longitudinal": False,
                 "lateral": True,
-                "target_speeds": [0, 2, 4]
+                "target_speeds": [0, 5, 10]
             },
             "simulation_frequency": 15,
             "policy_frequency": 5,
@@ -44,9 +46,9 @@ class CustomRoadEnv(AbstractEnv):
             "lane_centering_reward": 1,
             "action_reward": -0.3,
             "controlled_vehicles": 1,
-            "other_vehicles": 1,
-            "screen_width": 600,
-            "screen_height": 600,
+            "other_vehicles": 7,
+            "screen_width": 1200,
+            "screen_height": 800,
             "centering_position": [0.5, 0.5],
         })
         return config
@@ -84,7 +86,7 @@ class CustomRoadEnv(AbstractEnv):
         self._make_vehicles()
 
     def _make_road(self):
-        road_network: RoadNetwork = RoadNetwork()
+        """road_network: RoadNetwork = RoadNetwork()
         single_nodes: List[str] = ["a", "b", "c", "d", "e", "f", "g"]
         nodes: List[tuple] = make_nodes(single_nodes=single_nodes)
         speed: int = 10
@@ -182,6 +184,108 @@ class CustomRoadEnv(AbstractEnv):
                                            speed_limit=speed))
 
         road = Road(network=road_network, record_history=self.config["show_trajectories"])
+        self.road = road"""
+        net = RoadNetwork()
+
+        # Set Speed Limits for Road Sections - Straight, Turn20, Straight, Turn 15, Turn15, Straight, Turn25x2, Turn18
+        speedlimits = [None, 15, 15, 15, 15, 15, 15, 15, 15]
+
+        # Initialise First Lane
+        lane = StraightLane([42, 0], [100, 0], line_types=(LineType.CONTINUOUS, LineType.STRIPED), width=5,
+                            speed_limit=speedlimits[1])
+        self.lane = lane
+
+        # Add Lanes to Road Network - Straight Section
+        net.add_lane("a", "b", lane)
+        net.add_lane("a", "b",
+                     StraightLane([42, 5], [100, 5], line_types=(LineType.STRIPED, LineType.CONTINUOUS), width=5,
+                                  speed_limit=speedlimits[1]))
+
+        # 2 - Circular Arc #1
+        center1 = [100, -20]
+        radii1 = 20
+        net.add_lane("b", "c",
+                     CircularLane(center1, radii1, np.deg2rad(90), np.deg2rad(-1), width=5,
+                                  clockwise=False, line_types=(LineType.CONTINUOUS, LineType.NONE),
+                                  speed_limit=speedlimits[2]))
+        net.add_lane("b", "c",
+                     CircularLane(center1, radii1 + 5, np.deg2rad(90), np.deg2rad(-1), width=5,
+                                  clockwise=False, line_types=(LineType.STRIPED, LineType.CONTINUOUS),
+                                  speed_limit=speedlimits[2]))
+
+        # 3 - Vertical Straight
+        net.add_lane("c", "d", StraightLane([120, -20], [120, -30],
+                                            line_types=(LineType.CONTINUOUS, LineType.NONE), width=5,
+                                            speed_limit=speedlimits[3]))
+        net.add_lane("c", "d", StraightLane([125, -20], [125, -30],
+                                            line_types=(LineType.STRIPED, LineType.CONTINUOUS), width=5,
+                                            speed_limit=speedlimits[3]))
+
+        # 4 - Circular Arc #2
+        center2 = [105, -30]
+        radii2 = 15
+        net.add_lane("d", "e",
+                     CircularLane(center2, radii2, np.deg2rad(0), np.deg2rad(-181), width=5,
+                                  clockwise=False, line_types=(LineType.CONTINUOUS, LineType.NONE),
+                                  speed_limit=speedlimits[4]))
+        net.add_lane("d", "e",
+                     CircularLane(center2, radii2 + 5, np.deg2rad(0), np.deg2rad(-181), width=5,
+                                  clockwise=False, line_types=(LineType.STRIPED, LineType.CONTINUOUS),
+                                  speed_limit=speedlimits[4]))
+
+        # 5 - Circular Arc #3
+        center3 = [70, -30]
+        radii3 = 15
+        net.add_lane("e", "f",
+                     CircularLane(center3, radii3 + 5, np.deg2rad(0), np.deg2rad(136), width=5,
+                                  clockwise=True, line_types=(LineType.CONTINUOUS, LineType.STRIPED),
+                                  speed_limit=speedlimits[5]))
+        net.add_lane("e", "f",
+                     CircularLane(center3, radii3, np.deg2rad(0), np.deg2rad(137), width=5,
+                                  clockwise=True, line_types=(LineType.NONE, LineType.CONTINUOUS),
+                                  speed_limit=speedlimits[5]))
+
+        # 6 - Slant
+        net.add_lane("f", "g", StraightLane([55.7, -15.7], [35.7, -35.7],
+                                            line_types=(LineType.CONTINUOUS, LineType.NONE), width=5,
+                                            speed_limit=speedlimits[6]))
+        net.add_lane("f", "g", StraightLane([59.3934, -19.2], [39.3934, -39.2],
+                                            line_types=(LineType.STRIPED, LineType.CONTINUOUS), width=5,
+                                            speed_limit=speedlimits[6]))
+
+        # 7 - Circular Arc #4 - Bugs out when arc is too large, hence written in 2 sections
+        center4 = [18.1, -18.1]
+        radii4 = 25
+        net.add_lane("g", "h",
+                     CircularLane(center4, radii4, np.deg2rad(315), np.deg2rad(170), width=5,
+                                  clockwise=False, line_types=(LineType.CONTINUOUS, LineType.NONE),
+                                  speed_limit=speedlimits[7]))
+        net.add_lane("g", "h",
+                     CircularLane(center4, radii4 + 5, np.deg2rad(315), np.deg2rad(165), width=5,
+                                  clockwise=False, line_types=(LineType.STRIPED, LineType.CONTINUOUS),
+                                  speed_limit=speedlimits[7]))
+        net.add_lane("h", "i",
+                     CircularLane(center4, radii4, np.deg2rad(170), np.deg2rad(56), width=5,
+                                  clockwise=False, line_types=(LineType.CONTINUOUS, LineType.NONE),
+                                  speed_limit=speedlimits[7]))
+        net.add_lane("h", "i",
+                     CircularLane(center4, radii4 + 5, np.deg2rad(170), np.deg2rad(58), width=5,
+                                  clockwise=False, line_types=(LineType.STRIPED, LineType.CONTINUOUS),
+                                  speed_limit=speedlimits[7]))
+
+        # 8 - Circular Arc #5 - Reconnects to Start
+        center5 = [43.2, 23.4]
+        radii5 = 18.5
+        net.add_lane("i", "a",
+                     CircularLane(center5, radii5 + 5, np.deg2rad(240), np.deg2rad(270), width=5,
+                                  clockwise=True, line_types=(LineType.CONTINUOUS, LineType.STRIPED),
+                                  speed_limit=speedlimits[8]))
+        net.add_lane("i", "a",
+                     CircularLane(center5, radii5, np.deg2rad(238), np.deg2rad(268), width=5,
+                                  clockwise=True, line_types=(LineType.NONE, LineType.CONTINUOUS),
+                                  speed_limit=speedlimits[8]))
+
+        road = Road(network=net, np_random=self.np_random, record_history=self.config["show_trajectories"])
         self.road = road
 
     def _make_vehicles(self) -> None:
@@ -193,32 +297,25 @@ class CustomRoadEnv(AbstractEnv):
         # Controlled vehicles
         self.controlled_vehicles = []
         for i in range(self.config["controlled_vehicles"]):
-            lane_index = ("a", "b", rng.integers(2)) if i == 0 else \
-                self.road.network.random_lane_index(rng)
+            lane_index = self.road.network.random_lane_index(rng)
+
             controlled_vehicle = self.action_type.vehicle_class.make_on_lane(self.road, lane_index, speed=None,
                                                                              longitudinal=rng.uniform(20, 50))
 
             self.controlled_vehicles.append(controlled_vehicle)
             self.road.vehicles.append(controlled_vehicle)
 
-        # Front vehicle
-        vehicle = IDMVehicle.make_on_lane(self.road, ("a", "b", lane_index[-1]),
-                                          longitudinal=rng.uniform(
-                                              low=0,
-                                              high=self.road.network.get_lane(("b", "c", 0)).length
-                                          ),
-                                          speed=0 + rng.uniform(high=3))
-        self.road.vehicles.append(vehicle)
 
         # Other vehicles
-        for i in range(rng.integers(self.config["other_vehicles"])):
-            random_lane_index = self.road.network.random_lane_index(rng)
+        for i in range(self.config["other_vehicles"]):
+            lane_indexes: list = [('i', 'a', 1), ('d', 'e', 1), ('h', 'i', 0), ('c', 'd', 1), ('g', 'h', 1), ('h', 'i', 0), ('b', 'c', 1), ('e', 'f', 0), ('d', 'e', 0)]
+            random_lane_index = lane_indexes[i]
             vehicle = IDMVehicle.make_on_lane(self.road, random_lane_index,
                                               longitudinal=rng.uniform(
                                                   low=0,
                                                   high=self.road.network.get_lane(random_lane_index).length
                                               ),
-                                              speed=0 + rng.uniform(high=3))
+                                              speed=3 + rng.uniform(high=8))
             # Prevent early collisions
             for v in self.road.vehicles:
                 if np.linalg.norm(vehicle.position - v.position) < 20:
